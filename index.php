7fe0851d9796f68097a381e0192e814282dad0e9
Gosselin Pierre <pierre.gosselin@orange.fr>
2019-07-23 17:18:33 +0200

    Navbar

--- index.php
+++ index.php
@@ -6,7 +6,39 @@
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     </head>
     <body>
-        <h1>Bienvenue sur le site <?php bloginfo('name'); ?></h1>
-        <p><?php bloginfo('description'); ?></p>
+
+        <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#"><?php bloginfo('name'); ?></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    </ul>
+                </div>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+        </header>
+
+        <div class="container">
+            <h1 class="text-center my-5">Bienvenue sur le site</h1>
+            <p><?php bloginfo('description'); ?></p>
+            
+        </div>
     </body>
 </html>
