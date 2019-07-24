bfe96d214d0d8be3e2f6d80b87a1b73ac6acf427
pierre gosselin <pierre.gosselin@orange.fr>
2019-07-24 11:01:07 +0200

    Wordpress loop

--- index.php
+++ index.php
@@ -2,8 +2,31 @@
 // Equivalent d'un include "header.php"
 get_header();
 ?>
-            <h1 class="text-center my-5">Bienvenue sur le site</h1>
-            <p><?php bloginfo('description'); ?></p>
-            
+
+<h1 class="text-center my-5">Bienvenue sur le site</h1>
+<p><?php bloginfo('description'); ?></p>
+<div class="content">
+
+    <?php 
+    if (have_posts())
+    {
+    // S'il y a des articles, on exécute cette partie -->
+        while (have_posts())
+        {
+            //Pour chaque article, on affiche ceci
+            the_post();
+            echo "<h2>".the_title()."</h2><br>";
+            echo "<p>".the_content()."</p>";
+
+        }
+    }
+    else
+    {
+    // S'il n'y a pas d'articles, on affiche ceci
+
+    }
+    ?>
+
+</div>
 <?php
 get_footer();
